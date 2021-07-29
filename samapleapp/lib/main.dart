18170d7e1b44c5c70479bdbd381b0ebe1a21a495
Rahul <rahulyadav143118@gmail.com>
2021-07-29 21:58:11 +0530

    Day4Complete

--- samapleapp/lib/main.dart
+++ samapleapp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:samapleapp/pages/home_page.dart';
 import 'package:samapleapp/pages/login_pages.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,12 +16,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home:HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      primaryTextTheme: GoogleFonts.latoTextTheme()),
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/login",
       routes: {
-        "/":(context)=>LoginPage(),
-        "/login":(context)=> HomePage()
+        "/login":(context)=>LoginPage(),
+        "/home":(context)=> HomePage()
       },
     );
 
--- samapleapp/lib/pages/login_pages.dart
+++ samapleapp/lib/pages/login_pages.dart
@@ -4,14 +4,40 @@ class LoginPage  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text("Login Page",style: TextStyle(
-        fontSize:40,
-        color: Colors.deepPurple,
-        fontWeight: FontWeight.bold
-      ),
-      // textScaleFactor: 3.0,
-      ),),
-      
+      color: Colors.white,
+child: Column(children: [Image.asset("assets/images/img1.png",fit: BoxFit.cover,
+),
+SizedBox(height: 20,),
+Text("Welcome",style:
+ TextStyle(fontSize:24 ,
+ fontWeight: FontWeight.bold),
+ ),
+SizedBox(height: 20,),
+Padding(
+  padding: const EdgeInsets.all(16.0),
+  child:   Column(children: [TextFormField(decoration: InputDecoration(hintText: "Enter User Name",labelText: "UserName"),),
+  
+  TextFormField(obscureText: true,decoration: InputDecoration(hintText: "Enter Password",labelText: "Password"),
+  
+  ),
+  SizedBox(height: 20,),
+  ElevatedButton(onPressed: ()
+  {
+    print("Hello Rahul")
+
+  }, child: Text("Login"),
+  style: TextButton.,)],
+  
+  
+  ),
+
+
+)
+
+
+
+],),
     );
+      
   }
 }
