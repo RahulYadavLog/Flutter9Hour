a6b3383fd27f22c08f6e6714b4391c94b3c29541
Rahul <rahulyadav143118@gmail.com>
2021-07-31 16:20:08 +0530

    Day15Completed

--- samapleapp/lib/pages/home_page.dart
+++ samapleapp/lib/pages/home_page.dart
@@ -43,12 +43,42 @@ final String name="Rahul";
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child:(CatlogModel.items.isEmpty)?Center (child: CircularProgressIndicator(),):
-        ListView.builder(
-          itemCount: CatlogModel.items.length,
-          itemBuilder: (context,index)
-          {
-            return ItemWidget(item: CatlogModel.items[index],);
-          },),
+//  ListView.builder(
+//           itemCount: CatlogModel.items.length,
+//           itemBuilder: (context,index)
+//           {
+//             return ItemWidget(item: CatlogModel.items[index],);
+//           },)
+
+
+
+
+       GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+         crossAxisCount: 2,
+         mainAxisSpacing: 20,
+         crossAxisSpacing: 16),
+         
+         itemBuilder: (context,index)
+       {
+          final item=CatlogModel.items[index];
+         return Card(
+           clipBehavior: Clip.antiAlias,
+           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+           child: GridTile(child: Image.network(item.image),
+           header:Container(child: Text( item.name,style: TextStyle(color: Colors.white),),
+           padding:const EdgeInsets.all(12),
+           decoration: BoxDecoration(
+             color: Colors.deepPurple
+           ),),
+           footer:Container(child: Text( item.price.toString(),style: TextStyle(color: Colors.white),),
+           padding:const EdgeInsets.all(12),
+           decoration: BoxDecoration(
+             color: Colors.deepPurple
+           ),)),
+                 
+         );
+       },
+       itemCount: CatlogModel.items.length,),
       ),
     drawer: MyDrawer(),
     );
