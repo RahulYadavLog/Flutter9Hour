3d54bffeb53ae5ef3c1af5552f33a99141b85846
Rahul <rahulyadav143118@gmail.com>
2021-07-31 11:46:52 +0530

    day13 completed

--- samapleapp/lib/pages/home_page.dart
+++ samapleapp/lib/pages/home_page.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:samapleapp/models/catlog.dart';
 import 'package:samapleapp/widgets/drawer.dart';
 import 'package:samapleapp/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
 final int day=30;
+
 final String name="Rahul";
+@override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData() async
+  {
+   var catlogJson=await rootBundle.loadString("assets/files/catlog.json");
+   var decodData=jsonDecode(catlogJson);
+   var productData=decodData["products"];
+   print(catlogJson);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList=List.generate(50, (index) => CatlogModel.items[0]);
